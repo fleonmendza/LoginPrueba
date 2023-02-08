a6b2cc509de398509634bc72e43df185ab4381c4
fleonmendza <fleonmendza@gmail.com>
2023-02-07 21:45:10 -0600

    tabla

--- main.js
+++ main.js
@@ -1,32 +1,41 @@
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
-const regexPass = /^(?=.*[\#\$\.\@\<\>\!\¿\?\&\%\¡])(?=.*[A-Z])(?=.*[a-z])\S{16}$/gm;
-const regexMail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/gm;
-const regexName = /^[a-zA-Z\s]{1,40}$/gm;
+const regexPass = /^(?=.*[\#\$\.\@\<\>\!\¿\?\&\%\¡])(?=.*[A-Z])(?=.*[a-z])\S{16}$/g;
+const regexMail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/g;
+const regexName = /^[a-zA-Z\s]{1,25}$/g;
 
 const validarForm = (e) => {
-    if(e.target.name == "name") {
-        if(regexMail.test(e.target.value)){
-            console.log("correo");
-        }else if(regexName.test(e.target.value)){
-            console.log("mobre")
-        }else{
-            console.log("no vlaido")
-        }
-    }else if(e.target.name == "password"){
-        console.log("paswoeerddd")
-    }
+    switch(e.target.name){
+        case "name":
+            if(regexMail.test(e.target.value)){
+                console.log("correo");
+                document.getElementsById('name').classList.add('formulario_input-correo:hover');
+            }else if(regexName.test(e.target.value)){
+                document.getElementsById('name').classList.add('formulario_input-name:hover');
+                console.log("mobre")
+            }else{
+                console.log("no vlaido");
+            }
+        break;
+        case "password":
+            if(regexPass.test(e.target.value)){
+                console.log("pass corecta");
+            }else{
+                console.log("pass mala");
+            }
+        break;
+    }      
 }
 
 inputs.forEach((input) =>{
-    //input.addEventListener('keyup', validarForm)
-    input.addEventListener('input', validarForm)
+    input.addEventListener('change', validarForm);
+    input.addEventListener('blur', validarForm)
 })
 
 
 const text = "DiDs12%3456789#@";
-const correo = "abram.awelo@prestalana.com";
+const correo = "abram.awelo@prestalana.com.mx";
 
 
 if(regexPass.test(text)){
