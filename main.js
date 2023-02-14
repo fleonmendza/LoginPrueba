0e80c6ac48755a9aa12b3916706bab94298576b0
fleonmendza <fleonmendza@gmail.com>
2023-02-14 17:29:21 -0600

    ADD Validation

--- main.js
+++ main.js
@@ -5,19 +5,26 @@ const regexPass = /^(?=.*[\#\$\.\@\<\>\!\¿\?\&\%\¡\/])(?=.*[A-Z])(?=.*[a-z])\S
 const regexMail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/g;
 const regexName = /^[a-zA-Z\s]{1,25}$/g;
 
+let user ;
+let pass ;
+
 const validarForm = (e) => {
+
     switch(e.target.name){
         case "name":
             if(regexMail.test(e.target.value)){
                 document.getElementById('name').classList.replace('formulario_input-name', 'formulario_input');
                 document.getElementById('name').classList.replace('formulario_input','formulario_input-correo');
                 console.log("correo"); 
+                user = true;
             }if (regexName.test(e.target.value)){
                 document.getElementById('name').classList.replace('formulario_input-correo', 'formulario_input');
                 document.getElementById('name').classList.replace('formulario_input','formulario_input-name');
                 console.log("nombre")
+                user = true;
             }else{
                 console.log("no valido");
+                user = false;
             }
         break;
         case "password":
@@ -25,21 +32,32 @@ const validarForm = (e) => {
                 document.getElementById('password').classList.replace('formulario_input_pass-error', 'formulario_input_pass');
                 document.getElementById('password').classList.replace('formulario_input_pass','formulario_input_pass-correcto');
                 console.log("pass corecta");
+                pass = true;
             }else{
                 document.getElementById('password').classList.replace('formulario_input_pass-correcto', 'formulario_input_pass');
                 document.getElementById('password').classList.replace('formulario_input_pass','formulario_input_pass-error');
                 console.log("pass mala");
+                pass = false;
             }
         break;
     }      
 }
 
+formulario.addEventListener("submit", e=>{
+    e.preventDefault()
+    if(user && pass){
+        window.location.href = "http://127.0.0.1:5501/tabla.html";
+        console.log("validacion correcta")
+    }else{
+        alert("datos incorrectos");
+        console.log("validacion mala")
+    }
+})
+
 inputs.forEach((input) =>{
-   //input.addEventListener('change', validarForm);
-    //input.addEventListener('blur', validarForm);
-   // input.addEventListener('keyup', validarForm);
    input.addEventListener('input', validarForm);
 })
+
 
 
 const text = "DiDs12%3456789#@";
